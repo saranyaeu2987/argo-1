24940059c1fde1678e295a7a7862adb3f63dc9ad
Saranya Elumalai <elumalai.saranya@heb.com>
2020-10-06 16:47:02 -0500

    review suggessions

--- config/config.go
+++ config/config.go
@@ -7,7 +7,6 @@ import (
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"time"
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo-workflows/v3/workflow/common"
